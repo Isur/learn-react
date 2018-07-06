b7915191bfe2cc37712d57dbcd340745286c8a75
Isur <bednarczykartur96@gmail.com>
2018-07-06 09:25:21 +0200

    gitapi app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GitApp from './AppGitApi';
 import registerServiceWorker from './registerServiceWorker';
 
 
 
 
-ReactDOM.render(<App />, root);
+// ReactDOM.render(<App />, root);
+ReactDOM.render(<GitApp />, root);
 //ReactDOM.render(<App label="Do"/>, document.getElementById('root'));
 registerServiceWorker();
